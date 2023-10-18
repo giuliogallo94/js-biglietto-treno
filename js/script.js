e66b65fbc160b32b888a4e5b7d5d3a1f108f6a0d
giuliogallo94 <giuliogallo1994@gmail.com>
2023-10-18 15:09:56 +0200

    add: final output

--- js/script.js
+++ js/script.js
@@ -19,17 +19,28 @@ console.log(age);
 const standardPrice = kmToTravel * kmPrice;
 console.log(standardPrice);
 
-const underPrice = standardPrice - (standardPrice * 0, 2);
-const overPrice = standardPrice - (standardPrice * 0, 4);
+const underPrice = standardPrice - standardPrice * 0.2;
+const overPrice = standardPrice - standardPrice * 0.4;
 
 let finalPrice = "";
+let price = "Giulio";
 
 if (age < 18) {
   finalPrice = underPrice;
+  price = " con sconto del 20%";
 } else if (age > 65) {
   finalPrice = overPrice;
+  price = " con sconto del 40%";
 } else {
   finalPrice = standardPrice;
+  price = " con la tariffa standar";
 }
 
-console.log(finalPrice);
+console.log(finalPrice + price);
+
+// Output
+document.getElementById(
+  "finalPrice"
+).innerHTML = `Caro ${name} l'offerta che ti proponiamo è di €${
+  finalPrice + price
+}`;
